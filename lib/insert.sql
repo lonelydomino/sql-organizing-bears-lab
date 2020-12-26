238daebd3bde767401b592feb175338746d4bebb
Michael Martinez <zeroeclipse@live.com>
2020-12-25 17:35:29 -0800

    commit tree ddb3374362a4d1b940eb999a0cfe37b427734797

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'blue', 'cool', 0); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'blue', 'cool', 0); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'M', 'blue', 'cool', 0); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'M', 'cool', 'blue', 0); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'M', 'blue', 'cool', 0); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 3, 'M', 'blue', 'cool', 0); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'M', 'blue', 'cool', 0); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'M', 'blue', 'cool', 0); 
+
